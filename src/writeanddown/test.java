2578f6ee5db73c1bd8db66b1447ac914d8511051
liuchangling5 <liuchangling1995@163.com>
2018-04-25 21:00:25 +0800

    注释

--- src/writeanddown/test.java
+++ src/writeanddown/test.java
@@ -10,17 +10,17 @@ public class test {
 	public static void main(String args[]) throws IOException{
 		FileInputStream is=new FileInputStream("C:\\Users\\Administrator\\Desktop\\新建文本文档 (2).txt");
 		InputStreamReader isr=new InputStreamReader(is);
-		File file=new File("C:\\Users\\Administrator\\Desktop", "文档r.txt");
-		if(!file.exists()){
+		File file=new File("C:\\Users\\Administrator\\Desktop", "文档r.txt");//绝对路径
+		if(!file.exists()){//创建文件
 			file.createNewFile();
 		}
 		FileOutputStream os=new FileOutputStream(file);
 		OutputStreamWriter osr=new OutputStreamWriter(os);
 		char []buffer=new char[2056];
 		int c;
-		while((c=isr.read(buffer,0,buffer.length))!=-1)
+		while((c=isr.read(buffer,0,buffer.length))!=-1)//读取isr中的文字内容
 		{
-			osr.write(buffer, 0, c);
+			osr.write(buffer, 0, c);//将内容写入新文件中
 			osr.flush();
 		}
 		isr.close();
